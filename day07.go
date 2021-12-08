6970aa473307e0a9570b1dd414a04a8371f3e759
DESKTOP-AHD9183\RebeccaS <rebecca.sigmund@web.de>
2021-12-08 22:24:42 +0100

    solution for day 7

--- day07.go
+++ day07.go
@@ -1,15 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"math"
+	"sort"
+)
 
 func solveExercise7(fileName string) (solution int) {
 	lines := readFile(fileName)
-	fmt.Println(lines)
+	positions := strToInts(lines[0], ",")
+	sort.Ints(positions)
+	middlePosition := positions[len(positions)/2]
+	solution = calculateFuelLin(positions, middlePosition)
+	return
+}
+
+func calculateFuelLin(positions []int, middlePosition int) (fuel int) {
+	for _, position := range positions {
+		fuel += int(math.Abs(float64(position) - float64(middlePosition)))
+	}
 	return
 }
 
 func solveExercise7b(fileName string) (solution int) {
 	lines := readFile(fileName)
-	fmt.Println(lines)
+	positions := strToInts(lines[0], ",")
+	sort.Ints(positions)
+	solution = calculateFuelb(positions, 0)
+	for i := positions[0]; i <= positions[len(positions)-1]; i++ {
+		solution = getMin(solution, calculateFuelb(positions, i))
+	}
+	return
+}
+
+func calculateFuelb(positions []int, middlePosition int) (fuel int) {
+	for _, position := range positions {
+		distance := int(math.Abs(float64(position) - float64(middlePosition)))
+
+		fuel += (distance * (distance + 1)) / 2
+	}
 	return
 }
--- day07_test.go
+++ day07_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestExercise7(t *testing.T) {
-	expected := 0
+	expected := 37
 	actual := solveExercise7("file07_test.txt")
 	if expected != int(actual) {
 		t.Fatalf("Was not the right solution. Expected %d, but was actual %d", expected, actual)
@@ -13,7 +13,7 @@ func TestExercise7(t *testing.T) {
 }
 
 func TestExercise7b(t *testing.T) {
-	expected := 0
+	expected := 168
 	actual := solveExercise7b("file07_test.txt")
 	if expected != int(actual) {
 		t.Fatalf("Was not the right solution. Expected %d, but was actual %d", expected, actual)
